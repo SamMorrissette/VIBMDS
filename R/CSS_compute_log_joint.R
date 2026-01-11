41c88bb4c4b26ae081295691a064a7f9ec46bdfa
SamMorrissette <samuel.morrissette01@gmail.com>
2026-01-11 13:13:19 -0600

    improve log_lik_minibatch

--- R/CSS_compute_log_joint.R
+++ R/CSS_compute_log_joint.R
@@ -16,11 +16,6 @@ CSS_compute_log_joint <- function(dist_vec, z, prior_params, B, device) {
 
   p_x_tilde <- distr_normal(0, z$lambda$unsqueeze(2))$log_prob(z$x_tilde)$sum(dim = c(2, 3))
 
-  # p_omega <- torch_log_beta(z$omega$squeeze(), prior_params$a_omega, prior_params$b_omega)
-
-  #alpha_omega <- z$omega / (1 - z$omega)
-  #p_z_gates <- torch_log_binconcrete(z$z_gates, alpha_omega, prior_params$temp)$sum(dim = 2)
-
   p_z_gates <- torch_log_binconcrete(z$z_gates, prior_params$loc, prior_params$temp)$sum(dim = 2)
-  log_lik + p_sigma + p_lambda + p_x_tilde + p_z_gates #+ p_omega
+  log_lik + p_sigma + p_lambda + p_x_tilde + p_z_gates
 }
